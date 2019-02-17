061f95b25130f7d344e099b6323d493e0f135cc3
Joe <jphofmeister@gmail.com>
2019-02-17 03:00:47 -0500

    start migrating functions to child component

--- client/src/components/dashboard/FoodOnDate.js
+++ client/src/components/dashboard/FoodOnDate.js
@@ -3,12 +3,40 @@ import FoodIngredientTable from '../common/FoodIngredientTable';
 import Spinner from '../common/Spinner';
 import { Input } from 'reactstrap';
 import PropTypes from 'prop-types';
+import isEmpty from '../../validation/is-empty';
+import { connect } from 'react-redux';
+import { addDay, deleteFoodFromDay } from '../../actions/dayActions';
 
 class FoodOnDate extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      date: new Date().toISOString().slice(0, 10),
+      foodEaten: [],
+      calories: 0,
+      errors: {}
+    }
+
     this.handleChangeDate = this.handleChangeDate.bind(this);
-    this.handleDeleteFoodFromDay = this.handleDeleteFoodFromDay.bind(this);
+    //this.handleDeleteFoodFromDay = this.handleDeleteFoodFromDay.bind(this);
+    this.onDeleteFoodFromDay = this.onDeleteFoodFromDay.bind(this);
+    this.subtractCalories = this.subtractCalories.bind(this);
+    this.updateDay = this.updateDay.bind(this);
+  }
+
+  componentDidMount() {
+    const { day } = this.props;
+    if (day !== null) {
+      let foodEatenIds = !isEmpty(day.foodEaten) ? day.foodEaten.map(food => food._id) : [];
+      day.date = !isEmpty(day.date) ? day.date.slice(0, 10) : new Date().toISOString().slice(0, 10);
+      day.calories = !isEmpty(day.calories) ? day.calories : 0;
+
+      this.setState({
+        date: day.date,
+        foodEaten: foodEatenIds,
+        calories: day.calories
+      }, () => console.log(this.state));
+    }
   }
 
   handleChangeDate(e) {
@@ -16,22 +44,77 @@ class FoodOnDate extends Component {
     this.props.onChange(newDate);
   }
 
-  handleDeleteFoodFromDay(id, calories) {
-    this.props.onDeleteClick(id, calories);
+  onDeleteFoodFromDay(id, foodCal) {
+    //remove food from day in database
+    let date = this.state.date;
+    this.props.deleteFoodFromDay(date, id);
+    this.subtractCalories(foodCal);
+
+    // if (this.state.foodEaten.length > 1) {
+    //   //get index
+    //   const removeIndex = this.state.foodEaten
+    //     .map(food => food.toString())
+    //     .indexOf(id);
+
+    //   //remove food from foodEaten state
+    //   this.setState((prevState) => ({
+    //     foodEaten: [...prevState.foodEaten.slice(0, removeIndex), ...prevState.foodEaten.slice(removeIndex + 1)]
+    //   }));
+
+    //   this.subtractCalories(foodCal);
+    // } else {
+    //   this.setState({
+    //     foodEaten: [],
+    //     calories: 0
+    //   }, () => this.updateDay())
+    // }
   }
 
+  subtractCalories(foodCal) {
+    let newCalories = this.state.calories - foodCal;
+    this.setState({
+      calories: newCalories
+    }, () => this.updateDay());
+  }
+
+  updateDay() {
+    const dayData = {
+      date: this.state.date,
+      foodEaten: this.state.foodEaten,
+      calories: this.state.calories
+    }
+
+    this.props.addDay(dayData);
+
+    //console.log(`this is the state after updateDay is called ${this.state}`);
+  }
+
+  // handleDeleteFoodFromDay(id, calories) {
+  //   this.props.onDeleteClick(id, calories);
+  // }
+
   render() {
-    //if (this.props.day) {
-    const { day, loading } = this.props;
-    const { foodAddedToDay } = this.props;
+    const { day, loading, foods } = this.props;
     const headings = ['Food', 'Calories', ' '];
+
+    let foodAddedToDay = foods.map(food => {
+      if (!this.state.foodEaten.includes(food._id)) return null;
+
+      return ({
+        _id: food._id,
+        name: food.name,
+        calories: food.calories
+      });
+    });
+
+    let filteredFoodAddedToDay = foodAddedToDay.filter(Boolean);
 
     let dayContent;
 
-    if (foodAddedToDay === null || foodAddedToDay === undefined || loading) {
-      dayContent = "No food added today"
+    if (filteredFoodAddedToDay === null || filteredFoodAddedToDay === undefined || loading) {
+      dayContent = "No food added today";
     } else {
-      dayContent = <FoodIngredientTable items={foodAddedToDay} headings={headings} onDeleteClick={this.handleDeleteFoodFromDay} />
+      dayContent = <FoodIngredientTable items={filteredFoodAddedToDay} headings={headings} onDeleteClick={this.onDeleteFoodFromDay} />
     }
 
     return (
@@ -41,10 +124,10 @@ class FoodOnDate extends Component {
           name="date"
           type="date"
           onChange={this.handleChangeDate}
-          value={this.props.date} />
+          value={this.state.date} />
         {dayContent}
         <div>
-          Total Calories: {this.props.calories}
+          Total Calories: {this.state.calories}
         </div>
       </div>
     )
@@ -55,4 +138,8 @@ FoodOnDate.propTypes = {
   onDeleteFoodFromDay: PropTypes.func
 }
 
-export default FoodOnDate;
+const mapStateToProps = state => ({
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, { addDay, deleteFoodFromDay })(FoodOnDate);
--- client/src/components/dashboard/ManageFoodForDate.js
+++ client/src/components/dashboard/ManageFoodForDate.js
@@ -6,6 +6,7 @@ import { addDay, getDay, deleteFoodFromDay } from '../../actions/dayActions';
 import { getFoods, deleteFood } from '../../actions/foodActions';
 
 import isEmpty from '../../validation/is-empty';
+import Spinner from '../common/Spinner';
 
 import FoodOnDate from './FoodOnDate';
 import FoodCol from '../food/FoodCol';
@@ -23,94 +24,104 @@ class ManageFoodForDate extends Component {
     }
 
     this.onChangeDate = this.onChangeDate.bind(this);
-    this.onDeleteFoodFromDay = this.onDeleteFoodFromDay.bind(this);
+    //this.onDeleteFoodFromDay = this.onDeleteFoodFromDay.bind(this);
     this.onDeleteFood = this.onDeleteFood.bind(this);
-    this.onAddFoodToDay = this.onAddFoodToDay.bind(this);
-    this.updateDay = this.updateDay.bind(this);
-    this.subtractCalories = this.subtractCalories.bind(this);
+    //this.onAddFoodToDay = this.onAddFoodToDay.bind(this);
+    //this.updateDay = this.updateDay.bind(this);
+    //this.subtractCalories = this.subtractCalories.bind(this);
   }
 
   componentDidMount() {
     this.props.getFoods();
-    this.props.getDay(this.state.date);
+    let todaysDate = new Date().toISOString().slice(0, 10)
+    this.props.getDay(todaysDate);
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    const { day } = this.props.day;
+  // componentDidUpdate(prevProps, prevState) {
+  //   const { day } = this.props.day;
 
-    let newDate = this.state.date;
+  //   let newDate = this.state.date;
 
-    if (day !== null) {
-      if (!isEmpty(day.calories)) {
-        if (prevState.calories !== day.calories) {
-          let testFoodEaten = !isEmpty(day.foodEaten) ? day.foodEaten.map(food => food._id) : [];
-          day.date = !isEmpty(day.date) ? day.date : newDate;
-          day.calories = !isEmpty(day.calories) ? day.calories : 0;
+  //   if (day !== null) {
+  //     if (!isEmpty(day.calories)) {
+  //       if (prevState.calories !== day.calories) {
+  //         let testFoodEaten = !isEmpty(day.foodEaten) ? day.foodEaten.map(food => food._id) : [];
+  //         day.date = !isEmpty(day.date) ? day.date : newDate;
+  //         day.calories = !isEmpty(day.calories) ? day.calories : 0;
 
-          this.setState({
-            date: day.date.slice(0, 10),
-            foodEaten: testFoodEaten,
-            calories: day.calories
-          });
-        }
-      }
-    } else {
-      if (this.state.calories !== prevState.calories) {
-        this.setState({
-          foodEaten: [],
-          calories: 0
-        });
-      }
-    }
-  }
+  //         this.setState({
+  //           date: day.date.slice(0, 10),
+  //           foodEaten: testFoodEaten,
+  //           calories: day.calories
+  //         }, () => console.log(this.state));
+  //       }
+  //     }
+  //   } else {
+  //     if (this.state.calories !== 0) {
+  //       this.setState({
+  //         foodEaten: [],
+  //         calories: 0
+  //       });
+  //     }
+  //   }
+  // }
 
   onChangeDate(newDate) {
-    this.setState({ date: newDate }, () => this.props.getDay(newDate));
+    //this.setState({ date: newDate }, () => this.props.getDay(newDate));
+    this.props.getDay(newDate);
   }
 
-  onAddFoodToDay(id, foodCal) {
-    let newCalories = this.state.calories + foodCal;
+  // onAddFoodToDay(id, foodCal) {
+  //   let newCalories = this.state.calories + foodCal;
 
-    this.setState({
-      foodEaten: [...this.state.foodEaten, id],
-      calories: newCalories
-    }, () => this.updateDay());
-  }
+  //   this.setState({
+  //     foodEaten: [...this.state.foodEaten, id],
+  //     calories: newCalories
+  //   }, () => this.updateDay());
+  // }
 
-  onDeleteFoodFromDay(id, foodCal) {
-    //remove food from day in database
-    let date = this.state.date;
-    this.props.deleteFoodFromDay(date, id);
+  // onDeleteFoodFromDay(id, foodCal) {
+  //   //remove food from day in database
+  //   let date = this.state.date;
+  //   this.props.deleteFoodFromDay(date, id);
 
-    //get index
-    const removeIndex = this.state.foodEaten
-      .map(food => food.toString())
-      .indexOf(id);
+  //   if (this.state.foodEaten.length > 1) {
+  //     //get index
+  //     const removeIndex = this.state.foodEaten
+  //       .map(food => food.toString())
+  //       .indexOf(id);
 
-    //remove food from foodEaten state
-    this.setState((prevState) => ({
-      foodEaten: [...prevState.foodEaten.slice(0, removeIndex), ...prevState.foodEaten.slice(removeIndex + 1)]
-    }));
+  //     //remove food from foodEaten state
+  //     this.setState((prevState) => ({
+  //       foodEaten: [...prevState.foodEaten.slice(0, removeIndex), ...prevState.foodEaten.slice(removeIndex + 1)]
+  //     }));
 
-    this.subtractCalories(foodCal);
-  }
+  //     this.subtractCalories(foodCal);
+  //   } else {
+  //     this.setState({
+  //       foodEaten: [],
+  //       calories: 0
+  //     }, () => this.updateDay())
+  //   }
+  // }
 
-  subtractCalories(foodCal) {
-    let newCalories = this.state.calories - foodCal;
-    this.setState({
-      calories: newCalories
-    }, () => this.updateDay())
-  }
+  // subtractCalories(foodCal) {
+  //   let newCalories = this.state.calories - foodCal;
+  //   this.setState({
+  //     calories: newCalories
+  //   }, () => this.updateDay())
+  // }
 
-  updateDay() {
-    const dayData = {
-      date: this.state.date,
-      foodEaten: this.state.foodEaten,
-      calories: this.state.calories
-    }
+  // updateDay() {
+  //   const dayData = {
+  //     date: this.state.date,
+  //     foodEaten: this.state.foodEaten,
+  //     calories: this.state.calories
+  //   }
 
-    this.props.addDay(dayData);
-  }
+  //   this.props.addDay(dayData);
+  //   console.log(`this is the state after updateDay is called ${this.state}`);
+  // }
 
   onDeleteFood(id) {
     this.props.deleteFood(id);
@@ -120,28 +131,40 @@ class ManageFoodForDate extends Component {
     const { day, loading } = this.props.day;
     const { foods } = this.props.food;
 
-    let foodAddedToDay = foods.map(food => {
-      if (!this.state.foodEaten.includes(food._id)) return null;
+    console.log(day);
 
-      return ({
-        _id: food._id,
-        name: food.name,
-        calories: food.calories
-      });
-    });
+    // let foodAddedToDay = foods.map(food => {
+    //   if (!this.state.foodEaten.includes(food._id)) return null;
 
-    let filteredFoodAddedToDay = foodAddedToDay.filter(Boolean);
+    //   return ({
+    //     _id: food._id,
+    //     name: food.name,
+    //     calories: food.calories
+    //   });
+    // });
+
+    // let filteredFoodAddedToDay = foodAddedToDay.filter(Boolean);
+
+    let foodOnDateContent;
+
+    if (loading) {
+      foodOnDateContent = <Spinner />;
+    } else {
+      foodOnDateContent = <FoodOnDate
+        day={day}
+        //date={this.state.date}
+        //foodAddedToDay={day}
+        //calories={this.state.calories}
+        foods={foods}
+        onChange={this.onChangeDate}
+      //onDeleteClick={this.onDeleteFoodFromDay} 
+      />
+    }
 
     return (
       <div className="day-food-grid">
 
-        <FoodOnDate
-          day={day}
-          date={this.state.date}
-          foodAddedToDay={filteredFoodAddedToDay}
-          calories={this.state.calories}
-          onChange={this.onChangeDate}
-          onDeleteClick={this.onDeleteFoodFromDay} />
+        {foodOnDateContent}
 
         <div className="food card-style">
           <h3>Food</h3>
