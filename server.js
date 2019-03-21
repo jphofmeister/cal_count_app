734e7da81540b9567992f33e42569d5314e77c45
Joe <jphofmeister@gmail.com>
2019-03-21 16:33:25 -0400

    prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const ingredients = require('./routes/api/ingredients');
 const food = require('./routes/api/food');
@@ -27,6 +28,16 @@ mongoose
 app.use('/api/ingredients', ingredients);
 app.use('/api/food', food);
 app.use('/api/day', day);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
